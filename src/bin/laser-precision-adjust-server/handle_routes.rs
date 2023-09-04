fd4ac11ed45dc4a0ec323038c2d6520416f7b272
tolyan <tolyan@sctbelpa>
2023-09-04 17:39:01 +0300

    work - chart

--- src/bin/laser-precision-adjust-server/handle_routes.rs
+++ src/bin/laser-precision-adjust-server/handle_routes.rs
@@ -2,10 +2,31 @@ use axum::{extract::State, response::IntoResponse};
 use axum_template::{Key, RenderHtml};
 use laser_precision_adjust::Config;
 
+use serde::Serialize;
+
 use crate::AppEngine;
 
 pub(super) async fn handle_work(State(engine): State<AppEngine>) -> impl IntoResponse {
-    RenderHtml(Key("work".to_owned()), engine, ())
+    #[derive(Serialize)]
+    struct Rezonator {
+        pub f_start: f32,
+        pub f_end: f32,
+    }
+
+    #[derive(Serialize)]
+    struct Model {
+        pub resonators: Vec<Rezonator>,
+    }
+
+    let mut resonators = vec![];
+    for i in 0..16 {
+        resonators.push(Rezonator {
+            f_start: i as f32 * 100.0,
+            f_end: (i + 1) as f32 * 100.0,
+        });
+    }
+
+    RenderHtml(Key("work".to_owned()), engine, Model { resonators })
 }
 
 pub(super) async fn handle_stat(State(engine): State<AppEngine>) -> impl IntoResponse {
@@ -17,12 +38,10 @@ pub(super) async fn handle_config(
     State(config): State<Config>,
     State(config_file): State<std::path::PathBuf>,
 ) -> impl IntoResponse {
-    use serde::Serialize;
-
     #[derive(Serialize)]
     struct ConfigModel {
-        config_file: String,
-        config: Config,
+        pub config_file: String,
+        pub config: Config,
     }
 
     let model: ConfigModel = ConfigModel {
--- src/bin/laser-precision-adjust-server/wwwroot/ts/work.ts
+++ src/bin/laser-precision-adjust-server/wwwroot/ts/work.ts
@@ -1,4 +1,79 @@
+declare var uv: any;
 
 // on page loaded jquery
 $(() => {
+    const graphdef = {
+        categories : ['uvCharts', 'Matisse', 'SocialByWay'],
+        dataset : {
+            'uvCharts' : [
+                { name : '2008', value: 15},
+                { name : '2009', value: 28},
+                { name : '2010', value: 42},
+                { name : '2011', value: 88},
+                { name : '2012', value: 100},
+                { name : '2013', value: 143}
+            ],
+            'Matisse' : [
+                { name : '2008', value: 15},
+                { name : '2009', value: 28},
+                { name : '2010', value: 42},
+                { name : '2011', value: 88},
+                { name : '2012', value: 100},
+                { name : '2013', value: 143}
+            ],	
+            'SocialByWay' : [
+                { name : '2008', value: 15},
+                { name : '2009', value: 28},
+                { name : '2010', value: 42},
+                { name : '2011', value: 88},
+                { name : '2012', value: 100},
+                { name : '2013', value: 143}
+            ]
+        }
+    };
+    
+    const chartconfig = {
+        graph: {
+            //custompalette: palete,
+            bgcolor: 'none',
+            //max: 64,
+            //min: 0
+        },
+        /*
+        dimension: {
+            height: 20
+        },
+        */
+        margin: {
+            top: 1,
+            bottom: 1,
+            left: 1,
+            right: 1
+        },
+        axis: {
+            showticks: false,
+            showsubticks: false,
+            showtext: false,
+            showhortext: false,
+            showvertext: false
+        },
+        frame: {
+            bgcolor: 'none'
+        },
+        legend: {
+            showlegends: false
+        },
+        label: {
+            postfix: ' Бит',
+            fontfamily: 'PT Sans'
+        },
+        effects: {
+            duration: 100,
+        },
+        tooltip: {
+            format: '%c: %v бит'
+        }
+    };
+
+    var graph = new uv.chart('Line', graphdef, chartconfig);
 });
